5f0dfc928b9bb68f326273070a37f6bb11fe4f36
AnmolPlayzz <anmolisnotavailable@gmail.com>
2025-03-20 15:34:01 +0530

    Add input components with styling

--- src/components/Inputs/SingleInput/single-Input.tsx
+++ src/components/Inputs/SingleInput/single-Input.tsx
@@ -3,13 +3,11 @@
 import React from 'react';
 import styles from './style.module.css';
 
-export default function SingleInput({holder} : {holder: string}) {
+export default function SingleInput({holder, width} : {holder: string, width?: number}) {
   return (
     <div className={styles.border}>
-        <input type="text" className={styles.text} placeholder={holder} />
-        <div className={styles.outline}>
-
-        </div>
+        <input type="text" className={styles.text} placeholder={holder} style={{width: width ? `${width}px` : '100%'}} />
+        <div className={styles.outline}></div>
     </div>
   );
 }
--- src/components/Inputs/TextArea/textarea.tsx
+++ src/components/Inputs/TextArea/textarea.tsx
@@ -3,10 +3,10 @@
 import React from "react";
 import styles from "./styles.module.css";
 
-export default function TextArea({ holder }: { holder: string }) {
+export default function TextArea({ holder, width, height, wrap = true }: { holder: string, width?: number, height: number, wrap?: boolean }) {
   return (
-    <div className="textContainerT">
-      <textarea className={styles.textT} placeholder={holder} />
+    <div className={styles.textContainerT}>
+      <textarea className={styles.textT} placeholder={holder} wrap={wrap ? "soft" : "off"} style={{ width: width ? `${width}px` : '100%', height: `${height*34}px` }} />
     </div>
   );
 }
