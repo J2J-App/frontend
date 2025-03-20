6d7bf4b7dbd3da3d6d9e5128b52b16246a142479
AnmolPlayzz <anmolisnotavailable@gmail.com>
2025-03-20 15:52:15 +0530

    merging

---
+++ src/components/Switch/switch.js
@@ -0,0 +1,19 @@
+"use client"
+import { useState } from 'react';
+import styles from './Switch.module.css';
+
+export default function Switch(){
+
+  return (
+    <div>
+        <label className={`${styles.switch}`}>
+            <input className={`${styles.checked}`} type='checkbox'/>
+            <span className={`${styles.slider} ${styles.round}`}></span>
+        </label>
+        <label className={`${styles.switch}`}>
+            <input className={`${styles.checked}`} type='checkbox'/>
+            <span className={`${styles.slider}`}></span>
+        </label>
+    </div>
+  );
+};
