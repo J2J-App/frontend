609637a8a79f44320ceb580fe4fd21be387f7608
Alexis Amadei <alexis-amadei@scouts-europe.org>
2025-07-31 18:15:24 +0200

    Replace .env var for api url with public one. env file no longer required

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
-const API_URL = process.env.NEXT_PUBLIC_API_URL;
+const API_URL = "https://api.jeepedia.in/api";
+
 if (!API_URL) {
-    throw new Error("NEXT_PUBLIC_API_URL is not set");
+    throw new Error("API_URL is not set");
 }
+
 export default API_URL;
